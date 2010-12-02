09ce523284484fd9522dc64d8e3a5c1dbc50575b
James Selvakumar <james@mcruncher.com>
2010-12-02 17:26:41 +0800

    Updated the MEDIA_URL in settings.py

--- src/openlpwebapp/settings.py
+++ src/openlpwebapp/settings.py
@@ -14,7 +14,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = os.path.join(SITE_ROOT, 'db/songs.sqlite')             # Or path to database file if using sqlite3.
+#DATABASE_NAME = os.path.join(SITE_ROOT, 'db/songs.sqlite')             # Or path to database file if using sqlite3.
+DATABASE_NAME = '/home/james/.local/share/openlp/songs/songs.sqlite'
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
